85dca7081320c7ed500802992da50f1c2d50637d
mizzick <ds.kolyshev@gmail.com>
2017-12-11 19:05:38 +0700

    Platforms generator - hints

--- src/main/platforms/filter.js
+++ src/main/platforms/filter.js
@@ -13,6 +13,9 @@ module.exports = (() => {
     const ADG_STYLE_HACK = "adg_start_style_inject";
     const LOADED_SCRIPT_STRING = "loaded-script=\"true\"";
 
+    const PLATFORM_HINT_REGEXP = /(^| )PLATFORM\(([^)]+)\)/g;
+    const NOT_PLATFORM_HINT_REGEXP = /(.*)NOT_PLATFORM\(([^)]+)\)/g;
+
     /**
      * Parses rule hints
      *
@@ -42,14 +45,59 @@ module.exports = (() => {
     };
 
     /**
+     * Parses platforms by pattern
+     *
+     *
+     * @param hint    stripped hint
+     * @param pattern regexp
+     */
+    const parsePlatforms = function (hint, pattern) {
+        const result = [];
+
+        if (!hint) {
+            return result;
+        }
+
+        let match = pattern.exec(hint);
+        while (match !== null) {
+            let group = match[2];
+            let split = group.split(",");
+            for (let s of split) {
+                s = s.trim();
+                result.push(s);
+            }
+
+            match = pattern.exec(hint);
+        }
+
+        return result;
+    };
+
+    /**
      * Is rule supported with platform hint
      *
      * @param rule
      * @param platform
      */
     const isPlatformSupported = function (rule, platform) {
-        //TODO: Implement
-        return true;
+        const hint = rule.hint;
+        if (!hint || !hint.startsWith(HINT_MASK)) {
+            return true;
+        }
+
+        const stripped = hint.substring(HINT_MASK.length).trim();
+
+        const supportedPlatforms = parsePlatforms(stripped, PLATFORM_HINT_REGEXP);
+        const unsupportedPlatforms = parsePlatforms(stripped, NOT_PLATFORM_HINT_REGEXP);
+
+        if (!platform) {
+            return true;
+        }
+
+        const supported = supportedPlatforms.length === 0 || supportedPlatforms.indexOf(platform) >= 0;
+        const unsupported = unsupportedPlatforms.length > 0 && unsupportedPlatforms.indexOf(platform) >= 0;
+
+        return supported && !unsupported;
     };
 
     /**
