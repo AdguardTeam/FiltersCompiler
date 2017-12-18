0fea3fc8e82cd554812e4ef83099a06ee2a501f5
mizzick <ds.kolyshev@gmail.com>
2017-12-18 18:47:55 +0800

    review fixes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-/* globals require */
+/* globals require, __dirname */
 
 module.exports = (function () {
 
     'use strict';
 
-    let builder = require("./src/main/builder.js");
+    const path = require('path');
+    const builder = require("./src/main/builder.js");
+    const platformsConfig = path.join(__dirname, './platforms.json');
 
-    let compile = function (path, logPath, domainBlacklistFile, platformsPath, platformsConfig) {
+    let compile = function (path, logPath, domainBlacklistFile, platformsPath) {
         return builder.build(path, logPath, domainBlacklistFile, platformsPath, platformsConfig);
     };
 
