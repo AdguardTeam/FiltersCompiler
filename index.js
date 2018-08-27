0748ee9a2febc861f095c7d1eebe5fb439c02bca
mizzick <ds.kolyshev@gmail.com>
2018-08-27 13:57:43 +0300

    Fix #21 promise unhandled exceptions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* globals require, __dirname */
+/* globals require, __dirname, process */
 
 module.exports = (function () {
 
@@ -7,6 +7,11 @@ module.exports = (function () {
     const path = require('path');
     const builder = require("./src/main/builder.js");
     const platformsConfig = path.join(__dirname, './platforms.json');
+
+    process.on('unhandledRejection', error => {
+        //console.log('unhandledRejection', error.message);
+        throw error;
+    });
 
     let compile = function (path, logPath, domainBlacklistFile, platformsPath) {
         return builder.build(path, logPath, domainBlacklistFile, platformsPath, platformsConfig);
