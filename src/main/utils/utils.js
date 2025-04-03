7a230508273b2ea958a2fcbcbac338ab1626f5a6
jellizaveta <e.egorova@adguard.com>
2025-04-03 15:26:05 +0300

    remove unused func

--- src/main/utils/utils.js
+++ src/main/utils/utils.js
@@ -1,16 +1,4 @@
 /**
- * Removes array duplicates
- *
- * @param list
- * @returns {*}
- */
-// TODO: consider removing as not used
-const removeDuplicates = function (list) {
-    return list.filter((item, pos) => item.startsWith('!')
-        || list.indexOf(item) === pos);
-};
-
-/**
  * Returns filter ID from directory name if it has a number id,
  * otherwise returns 0.
  *
@@ -25,6 +13,5 @@ const getFilterIdFromDirName = (str) => {
 };
 
 export {
-    removeDuplicates,
     getFilterIdFromDirName,
 };
