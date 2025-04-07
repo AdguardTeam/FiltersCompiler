a91daebf255e11fd7eb32b77758926a960e1dcea
Slava Leleka <v.leleka@adguard.com>
2025-04-07 19:08:48 -0400

    add comment

--- tasks/build-schemas/index.js
+++ tasks/build-schemas/index.js
@@ -1,3 +1,12 @@
+/**
+ * @file
+ *
+ * Generates the JSON schema for filters.json and filters_i18n.json.
+ *
+ * Script is intended to simplify the process maintaining the schemas,
+ * e.g. when new filter id or locale is added, much less manual work is needed.
+ */
+
 const { generateFiltersI18nSchema } = require('./build-filters-i18n-schema');
 const { generateFiltersSchema } = require('./build-filters-schema');
 
