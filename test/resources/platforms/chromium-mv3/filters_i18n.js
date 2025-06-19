4ddfbc6cfbfece7c437a9ace76afd8692f211d2f
Elizaveta Egorova <elizavetaegorova@Elizaveta-Egorovas-MacBook-Air---DFXWKXFJH1.local>
2025-06-19 19:07:00 +0300

    move test to root folder

---
+++ test/resources/platforms/chromium-mv3/filters_i18n.js
@@ -0,0 +1,182 @@
+{
+	"groups": {
+		"1": {
+			"en": {
+				"name": "Adguard Filters",
+				"description": "Adguard Filters description"
+			},
+			"ru": {
+				"name": "Localized group1 name",
+				"description": "Localized group1 description"
+			}
+		},
+		"2": {
+			"en": {
+				"name": "EasyList",
+				"description": "EasyList description"
+			},
+			"ru": {
+				"name": "Localized group2 name",
+				"description": "Localized group2 description"
+			}
+		},
+		"3": {
+			"en": {
+				"name": "Other",
+				"description": "Other description"
+			},
+			"ru": {
+				"name": ""
+			}
+		}
+	},
+	"tags": {
+		"1": {
+			"en": {
+				"description": "Blocks ads",
+				"name": "Blocks ads"
+			},
+			"ko": {
+				"description": "Blocks ads"
+			}
+		},
+		"2": {
+			"en": {
+				"description": "Blocks trackers",
+				"name": "Blocks trackers"
+			},
+			"ko": {
+				"description": "Blocks trackers",
+				"name": "Blocks trackers"
+			}
+		},
+		"3": {
+			"en": {
+				"description": "Blocks social widgets",
+				"name": "Blocks social widgets"
+			}
+		},
+		"4": {
+			"en": {
+				"description": "Security-related filter",
+				"name": "Security-related filter"
+			},
+			"ko": {
+				"description": "Security-related filter",
+				"name": "Security-related filter"
+			}
+		},
+		"5": {
+			"en": {
+				"description": "Filter is for blocking annoyances",
+				"name": "Filter is for blocking annoyances"
+			},
+			"ko": {
+				"description": "Filter is for blocking annoyances",
+				"name": "Filter is for blocking annoyances"
+			}
+		},
+		"6": {
+			"en": {
+				"description": "Blocks EU cookie notices",
+				"name": "Blocks EU cookie notices"
+			}
+		},
+		"7": {
+			"en": {
+				"name": "Language-specific filter",
+				"description": "Language-specific filter"
+			}
+		},
+		"8": {
+			"en": {
+				"description": "Language-specific filter",
+				"name": "Language-specific filter"
+			}
+		},
+		"9": {
+			"en": {
+				"description": "Filter references EasyList",
+				"name": "Filter references EasyList"
+			}
+		},
+		"10": {
+			"en": {
+				"name": "Filter is recommended to be used with AdGuard",
+				"description": "Filter is recommended to be used with AdGuard"
+			},
+			"ko": {
+				"name": "Filter is recommended to be used with AdGuard",
+				"description": "Filter is recommended to be used with AdGuard"
+			}
+		}
+	},
+	"filters": {
+		"1": {
+			"en": {
+				"name": "Russian filter",
+				"description": "Adguard Russian filter. Filter that enables blocking of ads on websites in Russian."
+			},
+			"ru": {
+				"name": "Localized Russian filter",
+				"description": "Localized Adguard Russian filter. Filter that enables blocking of ads on websites in Russian."
+			}
+		},
+		"2": {
+			"en": {
+				"name": "AdGuard Base filter",
+				"description": "EasyList + AdGuard English filter. This filter is necessary for quality ad blocking."
+			},
+			"ko": {
+				"name": "AdGuard Base filter",
+				"description": "EasyList + AdGuard English filter. This filter is necessary for quality ad blocking."
+			},
+			"ru": {
+				"name": "Localized English filter",
+				"description": "Localized EasyList + Adguard English filter. Filter list that specifically removes adverts on English language websites."
+			}
+		},
+		"3": {
+			"en": {
+				"name": "Test Filter",
+				"description": "Test Filter description"
+			},
+			"ko": {
+				"name": "Test Filter",
+				"description": "Test Filter description"
+			},
+			"ru": {
+				"name": "Test Filter",
+				"description": "Test Filter description"
+			}
+		},
+		"4": {
+			"en": {
+				"name": "Directives Filter",
+				"description": "Directives Filter description."
+			},
+			"ko": {
+				"name": "Directives Filter",
+				"description": "Directives Filter description."
+			},
+			"ru": {
+				"name": "Directives Filter",
+				"description": "Directives Filter description."
+			}
+		},
+		"5": {
+			"en": {
+				"name": "Trust Level Test Filter",
+				"description": "Trust Level Test Filter description."
+			},
+			"ko": {
+				"name": "Trust Level Test Filter",
+				"description": "Trust Level Test Filter description."
+			},
+			"ru": {
+				"name": "Trust Level Test Filter",
+				"description": "Trust Level Test Filter description."
+			}
+		}
+	}
+}
--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         environment: 'node',
-        include: ['src/test/*.test.js'],
+        include: ['test/*.test.js'],
         watch: false,
+        silent: true,
     },
 });
