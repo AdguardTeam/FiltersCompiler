3f24d7d0e53a4c2a3940ce90487deb53fdfb5d64
Elizaveta <e.egorova@adguard.com>
2025-07-11 17:23:52 +0300

    Add cjs to build

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,17 @@ import copy from 'rollup-plugin-copy';
 
 export default {
     input: 'src/index.js',
-    output: {
-        dir: 'dist',
-        format: 'esm',
-    },
+    output: [
+        {
+            file: 'dist/index.js',
+            format: 'esm',
+        },
+        {
+            file: 'dist/index.cjs',
+            format: 'cjs',
+            exports: 'auto',
+        },
+    ],
     plugins: [
         copy({
             targets: [
